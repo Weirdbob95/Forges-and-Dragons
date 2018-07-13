2daa52778aa4300aeaad9e90b38d11e180588ce4
Weirdbob95 <rory.soiffer@comcast.net>
2018-07-13 08:28:49 -0700

    Slightly improved level generation

--- src/game/DungeonLevel.java
+++ src/game/DungeonLevel.java
@@ -3,34 +3,67 @@ package game;
 import engine.Behavior;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.function.Consumer;
 import org.joml.Vector2d;
 import util.Noise;
 
 public class DungeonLevel extends Behavior {
 
+    private static final int SIZE = 50;
+
     public Noise noise = new Noise(Math.random() * 1e6);
     public List<Wall> walls = new LinkedList();
+    public boolean[][] wallArray;
 
     @Override
     public void createInner() {
-        for (int i = 0; i < 50; i++) {
-            for (int j = 0; j < 50; j++) {
-                if (noise.perlin(i, j, .1) > .5) {
+        wallArray = new boolean[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                wallArray[i][j] = (noise.perlin(i, j, .1) > .5);
+
+            }
+        }
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (wallArray[i][j]) {
                     Wall w = new Wall();
                     w.position.position = new Vector2d(i, j).mul(32);
                     w.create();
                     walls.add(w);
-                } else if (Math.random() < .005) {
-                    Monster m = new Monster();
-                    m.position.position = new Vector2d(i, j).mul(32);
-                    m.create();
                 }
             }
+        }
+        for (int i = 0; i < 10; i++) {
+            placeObject(2, pos -> {
+                Monster m = new Monster();
+                m.position.position = pos;
+                m.create();
+            });
         }
     }
 
     @Override
     public void destroyInner() {
         walls.forEach(Wall::destroy);
+    }
+
+    public void placeObject(int squares, Consumer<Vector2d> placeCallback) {
+        while (true) {
+            int i = (int) (Math.random() * (SIZE + 1 - squares));
+            int j = (int) (Math.random() * (SIZE + 1 - squares));
+            boolean empty = true;
+            for (int i2 = i; i2 < i + squares; i2++) {
+                for (int j2 = j; j2 < j + squares; j2++) {
+                    if (wallArray[i2][j2]) {
+                        empty = false;
+                    }
+                }
+            }
+            if (empty) {
+                placeCallback.accept(new Vector2d(i + (squares - 1) / 2., j + (squares - 1) / 2.).mul(32));
+                break;
+            }
+        }
     }
 }
